56ea8c53fd0a24444364d5cd5fd1f693a750993a
Alejandro Garrocho Cruz <101363464+AlejandroGC19@users.noreply.github.com>
2024-10-02 09:18:41 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function searchExcuse() {
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class.",
+    "when I was sleeping.",
+    "while I was exercising.",
+    "during my lunch.",
+    "while I was praying."
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let who_index = Math.floor(Math.random() * who.length);
+  let action_index = Math.floor(Math.random() * action.length);
+  let what_index = Math.floor(Math.random() * what.length);
+  let when_index = Math.floor(Math.random() * when.length);
+
+  document.getElementById("excuse").innerHTML =
+    who[who_index] + action[action_index] + what[what_index] + when[when_index];
+}
+
+document.body.onload = searchExcuse;
+
+const newExcuse = document.getElementById("new-excuse");
+newExcuse.onclick = searchExcuse;
